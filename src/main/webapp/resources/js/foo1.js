5c1f65e02145a86bc6a1725f0be19fc63238d740
ChorokClaraLee <chorok1030@naver.com>
2022-06-07 14:48:07 +0900

    0607_yidain_mypage 테이블 라이브러리 수정
    mypage 테이블 라이브러리 수정

--- src/main/webapp/resources/js/foo1.js
+++ src/main/webapp/resources/js/foo1.js
@@ -9,11 +9,5 @@
  */
 
 
-  	$(document).ready(function() {
- 	    $('#foo-table1').DataTable( {
- 	    	bInfo : false,
- 	       	bPaginate : true,
- 	        displayLength : 10
- 	    } );
- 	} );	
+ 
    
